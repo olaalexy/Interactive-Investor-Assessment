576c985949e234cdfd83dcc9fc2c0c7d5bb0d975
Olaalexy <olaalexy22@gmail.com>
2026-04-18 13:29:03 +0100

    created test data

---
+++ utils/testData.ts
@@ -0,0 +1,39 @@
+export const testData = {
+  login: {
+    email: "demo.user@yopmail.com",
+    password: "Password@123"
+  },
+  products: {
+    searchKeyword: "Blue Top",
+    defaultProductName: "Blue Top"
+  },
+  checkout: {
+    comment: "Order placed via automated test flow."
+  },
+  payment: {
+    nameOnCard: "Demo User",
+    cardNumber: "4111111111111111",
+    cvc: "123",
+    expiryMonth: "12",
+    expiryYear: "2030"
+  },
+  registration: {
+    displayName: "Demo User",
+    account: {
+      password: "Password@123",
+      firstName: "Demo",
+      lastName: "User",
+      company: "Demo Corp",
+      address: "1 Test Street",
+      address2: "Suite 100",
+      country: "United States",
+      state: "California",
+      city: "Los Angeles",
+      zipcode: "90001",
+      mobile: "5551234567",
+      birthDay: "10",
+      birthMonth: "January",
+      birthYear: "1990"
+    }
+  }
+};
